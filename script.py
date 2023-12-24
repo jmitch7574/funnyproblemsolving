67ce85cbf3dc3b3a70bc206fa56ee658de825152
Jake Mitchell <j.mitchell7574@gmail.com>
2023-12-24 19:28:45 +0000

    error handling

--- script.py
+++ script.py
@@ -13,13 +13,31 @@ validInput = True
 if "." not in dob:
     validInput = False
 
-if validInput:
+dob_split = dob.split(".")
+
+# use a try except statement to see if date and month are given as valid ints
+try:
+    # separate the date and month into two separate variables
     dob_split = dob.split(".")
     date = int(dob_split[0])
     month = int(dob_split[1]) - 1
 
+# an error is thrown because either date or month cannot be parsed as an int
+except:
+    validInput = False
+
+
+# ensuring that date and month are in  valid ranges
+if not 0 < date <= 31:
+    validInput = False
+if not 0 < month <= 12:
+    validInput = False
+
+if validInput:
     firstSundayInMonth = int(code[month])
     dayInWeek = ((date + 7) - firstSundayInMonth) % 7
     weekDay = daysOfWeek[dayInWeek]
 
     print(weekDay)
+else:
+    print("Input is not valid")
